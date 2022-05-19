d4fec51963e180d33d60b585f9fc7929bff1ff2b
Stefano Proietti <stef.pro@hotmail.com>
2022-05-19 17:57:09 -0400

    configured frontend to fetch data via graphql

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,66 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+//Responsible for fetching the data
+export async function getStaticProps(context: any) {
+  const fetchParams: object = {
+    method: 'post',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `{
+      posts{
+        data{
+          attributes{
+            title,
+            heroImage{
+              data{
+                attributes{
+                  width,
+                  height
+                  name
+                }
+              }
+            }
+            IntroText,
+            coffeeReview,
+            locationReview,
+            secondImage{
+              data{
+                attributes{
+                  width,
+                  height
+                  name
+                }
+              }
+            }
+            finalVerdict,
+            category{
+              data{
+                attributes{
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+      `
+    })
+  }
+
+  const res = await fetch(`http://localhost:1337/graphql`, fetchParams);
+  const data = await res.json();
+
+  return {
+    props: data,
+  }
+}
+
+const Home: NextPage<{data: object}> = ({data}) => {
+  console.log(data);
   return (
     <div className={styles.container}>
       <Head>
