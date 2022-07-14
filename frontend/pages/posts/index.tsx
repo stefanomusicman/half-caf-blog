7668e83fe37036c30d76c39e567175b6391bcd4e
Stefano Proietti <stef.pro@hotmail.com>
2022-07-14 09:18:06 -0400

    search functionality added to posts page

--- frontend/pages/posts/index.tsx
+++ frontend/pages/posts/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import BlogCard from "../../components/BlogCard/BlogCard";
 import Footer from "../../components/Footer/Footer";
 import Navigation from "../../components/NavBar/Navigation";
@@ -19,19 +19,21 @@ export async function getStaticProps() {
   const BlogPosts: React.FC<{data: any}> = ({data}) => {
 
     const [searchTerm, setSearchTerm] = useState<string>(''); //Responsible for storing the search term
+    const [searchResults, setSearchResults] = useState<any>([]);
+    const [isSearching, setIsSearching] = useState<boolean>(false);
     const [posts, setPosts] = useState(data); //Responsible for storing the data being retrieved from strapi
     const [pageNumber, setPageNumber] = useState(1); //Responsible for storing the page number
 
     //Changes the page number based on which one user clicks on and loads appropriate content
     function pageLoadHandler(page: number): void {
       setPageNumber(page);
-      const fetchData = async () => {
+      const fetchPageData = async () => {
         const res = await fetch(`http://localhost:1337/api/posts?populate=heroImage,category&pagination[page]=${pageNumber}&pagination[pageSize]=6`);
         const data = await res.json();
   
         setPosts(data);
       }
-      fetchData();
+      fetchPageData();
     }
 
     //Creates the different page numbers
@@ -40,6 +42,27 @@ export async function getStaticProps() {
         return <div onClick={() => pageLoadHandler(i)} className={styles.pageNumber}>{i}</div>
       }
     }
+
+    // Fetchs Data based on search query
+    function getDataFromSearch(term: string) {
+      const fetchSearchData = async () => {
+        const res = await fetch(`http://localhost:1337/api/posts?populate=cardPhoto&filters[title][$containsi]=${term}`);
+        const data = await res.json();
+
+        setIsSearching(true);
+        setSearchResults(data.data);
+      }
+      fetchSearchData();
+    }
+
+    // Responsible for listening to every search term and carrying out a request to fetch appropriate data
+    useEffect(() => {
+      if(searchTerm.length === 0) {
+        setIsSearching(false);
+      } else {
+        getDataFromSearch(searchTerm);
+      }
+    }, [searchTerm])
 
     return(
       <Fragment>
@@ -53,7 +76,14 @@ export async function getStaticProps() {
                    placeholder="Search Post"/>
           </div>
           <div className={styles.primaryBodyContainer}>
-            {posts.data.map((item: any) => 
+            {!isSearching && posts.data.map((item: any) => 
+              <BlogCard introText={item.attributes.cardText} 
+                        image={item.attributes.cardPhoto.data.attributes.formats.medium.url} 
+                        key={item.id} 
+                        id={item.id} 
+                        title={item.attributes.title}
+                        dateCreated={item.attributes.createdAt}/>)}
+            {isSearching && searchResults.map((item: any) => 
               <BlogCard introText={item.attributes.cardText} 
                         image={item.attributes.cardPhoto.data.attributes.formats.medium.url} 
                         key={item.id} 
